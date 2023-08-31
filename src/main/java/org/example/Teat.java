decfad6d9639e83edb69d5883be34b26701afaef
Byron <1640709921@qq.com>
2023-08-31 21:16:55 +0800

    master主支

---
+++ src/main/java/org/example/Teat.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Teat {
+    public static void main(String[] args) {
+        System.out.println("测试的项目都有那些");
+        System.out.println("11111");
+        System.out.println("2222");
+        System.out.println("yangxx");
+    }
+}
